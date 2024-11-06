d297eedcbe95fd630f9dd3b7de0405396db796b5
Dev-LHY <dev.leehy@gmail.com>
2024-11-07 05:47:32 +0900

    feat(promotion): 프로모션 리스트 생성 추가
    - 프로모션 정보 리스트를 입력 받고 입력 받은 정보를 바탕으로 프로모션 리스트를 만드는 기능
    - 입력 받은 프로모션 정보는 parsePromotion에서 가공하여 프로모션 객체로 반환

--- src/main/java/store/model/Promotion.java
+++ src/main/java/store/model/Promotion.java
@@ -1,8 +1,17 @@
 package store.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Promotion {
+    private static final String DELIMITER_COMMA = ",";
+    private static final int NAME_INDEX = 0;
+    private static final int BUY_INDEX = 1;
+    private static final int GET_INDEX = 2;
+    private static final int START_DATE_INDEX = 3;
+    private static final int END_DATE_INDEX = 4;
+
     private final String name;
     private final int buy;
     private final int get;
@@ -15,6 +24,24 @@ public class Promotion {
         this.get = get;
         this.start_date = start_date;
         this.end_date = end_date;
+    }
+
+    public static List<Promotion> createPromotions(List<String> textLines) {
+        List<Promotion> promotions = new ArrayList<>();
+        for (String textLine : textLines) {
+            promotions.add(parsePromotion(textLine));
+        }
+        return promotions;
+    }
+
+    private static Promotion parsePromotion(String textLine) {
+        String[] fields = textLine.split(DELIMITER_COMMA);
+        String name = fields[NAME_INDEX];
+        int buy = Integer.parseInt(fields[BUY_INDEX]);
+        int get = Integer.parseInt(fields[GET_INDEX]);
+        LocalDate start_date = LocalDate.parse(fields[START_DATE_INDEX]);
+        LocalDate end_date = LocalDate.parse(fields[END_DATE_INDEX]);
+        return new Promotion(name, buy, get, start_date, end_date);
     }
 
     public String getName() {
