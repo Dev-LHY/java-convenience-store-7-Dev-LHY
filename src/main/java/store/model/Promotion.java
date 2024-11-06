875efed34d003a08ab6b3f9fb6520d0fd9499466
Dev-LHY <dev.leehy@gmail.com>
2024-11-07 05:29:16 +0900

    feat(promotion): 프로모션 추가

---
+++ src/main/java/store/model/Promotion.java
@@ -0,0 +1,50 @@
+package store.model;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDate start_date;
+    private final LocalDate end_date;
+
+    public Promotion(String name, int buy, int get, LocalDate start_date, LocalDate end_date) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.start_date = start_date;
+        this.end_date = end_date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "name='" + name + '\'' +
+                ", buy=" + buy +
+                ", get=" + get +
+                ", start_date=" + start_date +
+                ", end_date=" + end_date +
+                '}';
+    }
+}
