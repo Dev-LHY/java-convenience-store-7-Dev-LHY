a3a3c93b61c1f84a10298ceb8b1b4bee49c08e73
Dev-LHY <dev.leehy@gmail.com>
2024-11-07 05:24:26 +0900

    feat(product): 상품 리스트 생성 추가
    - 상품 정보 리스트를 입력 받고 입력 받은 정보를 바탕으로 상품 리스트를 만드는 기능
    - 입력 받은 상품 정보는 parseProduct에서 가공하여 상품 객체로 반환

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -1,6 +1,15 @@
 package store.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
+    private static final String DELIMITER_COMMA = ",";
+    private static final int NAME_INDEX = 0;
+    private static final int PRICE_INDEX = 1;
+    private static final int QUANTITY_INDEX = 2;
+    private static final int PROMOTION_INDEX = 3;
+
     private final String name;
     private final int price;
     private final String promotion;
@@ -11,6 +20,23 @@ public class Product {
         this.price = price;
         this.quantity = quantity;
         this.promotion = setPromotion(promotion);
+    }
+
+    public static List<Product> createProducts(List<String> textLines) {
+        List<Product> products = new ArrayList<>();
+        for (String textLine : textLines) {
+            products.add(parseProduct(textLine));
+        }
+        return products;
+    }
+
+    private static Product parseProduct(String textLine) {
+        String[] fields = textLine.split(DELIMITER_COMMA);
+        String name = fields[NAME_INDEX];
+        int price = Integer.parseInt(fields[PRICE_INDEX]);
+        int quantity = Integer.parseInt(fields[QUANTITY_INDEX]);
+        String promotion = fields[PROMOTION_INDEX];
+        return new Product(name, price, quantity, promotion);
     }
 
     public String getName() {
