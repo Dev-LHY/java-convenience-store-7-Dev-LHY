8ebf8c210d0d45745273adf8585ec3ea3e630fa0
Dev-LHY <dev.leehy@gmail.com>
2024-11-08 18:59:57 +0900

    feat(customer): 입력된 구매 품목 가공 추가
    - 사용자가 입력한 구매 품목을 "상품명-수량"으로 가공하여 리스트에 담고 리스트를 반환

--- src/main/java/store/model/Customer.java
+++ src/main/java/store/model/Customer.java
@@ -1,13 +1,29 @@
 package store.model;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Customer {
+    private static final String REGEX = "\\[(.*?)]";
+
     private final Map<String, Integer> shoppingCart = new LinkedHashMap<>();
 
     public Map<String, Integer> getShoppingCart() {
         return shoppingCart;
+    }
+
+    private List<String> parseItems(String items) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(items);
+        List<String> parseItems = new ArrayList<>();
+        while (matcher.find()) {
+            parseItems.add(matcher.group(1));
+        }
+        return parseItems;
     }
 
     @Override
