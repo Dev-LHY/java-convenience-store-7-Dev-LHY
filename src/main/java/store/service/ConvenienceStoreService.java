3e1b2b507e3a7f3612d8f02f915755cb10bb60fe
Dev-LHY <dev.leehy@gmail.com>
2024-11-10 01:21:22 +0900

    feat(convenienceStoreService): 프로모션 검증 추가
    - 프로모션 기간이 아닐 경우 해당 상품에 대한 검증 건너뜀
    - 프로모션을 받을 수 있는데 안 가져온 경우
    - 프로모션을 받을 수 없는데 가져온 경우

--- src/main/java/store/service/ConvenienceStoreService.java
+++ src/main/java/store/service/ConvenienceStoreService.java
@@ -1,19 +1,127 @@
 package store.service;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import store.exception.ExceptionMessage;
 import store.model.Customer;
+import store.model.Product;
 import store.model.Promotion;
 import store.model.StockManager;
-import store.util.TextFileLoader;
+import store.view.InputView;
+import store.view.OutputView;
 
 public class ConvenienceStoreService {
-    private final List<Promotion> promotions;
-    private final StockManager stock;
+    private final StockManager stockManager;
     private final Customer customer;
+    private final Map<String, Integer> shoppingCart;
+    private final Map<String, Promotion> promotions;
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
 
     public ConvenienceStoreService(Customer customer) {
-        this.promotions = Promotion.createPromotions(TextFileLoader.promotionsLoader());
-        this.stock = StockManager.getInstance();
+        this.stockManager = StockManager.getInstance();
         this.customer = customer;
+        this.shoppingCart = customer.getShoppingCart();
+        this.promotions = stockManager.getPromotions();
+    }
+
+    public void checkQuantity() {
+        for (Map.Entry<String, Integer> shoppingCartEntry : shoppingCart.entrySet()) {
+            int totalQuantityByName = stockManager.getTotalQuantityByName(shoppingCartEntry.getKey());
+            int shoppingCartQuantity = shoppingCartEntry.getValue();
+            if (totalQuantityByName < shoppingCartQuantity) {
+                throw new IllegalArgumentException(ExceptionMessage.EXCEED_QUANTITY.getMessage());
+            }
+        }
+    }
+
+    public void validatePromotion() {
+        Map<String, List<Product>> stock = stockManager.getStock();
+        String promotionName = null;
+        for (Map.Entry<String, Integer> shoppingCart : shoppingCart.entrySet()) {
+            List<Product> products = stock.get(shoppingCart.getKey());
+            Promotion promotion = promotions.get(products.getFirst().getPromotion());
+            if (checkPromotionTime(promotion.getStart_date(), promotion.getEnd_date())) {
+                continue;
+            }
+            validatePromotions(shoppingCart, products, promotionName);
+        }
+    }
+
+    private void validatePromotions(Entry<String, Integer> shoppingCart, List<Product> products, String promotionName) {
+        int promotionQuantity = stockManager.getPromotionQuantityByName(shoppingCart.getKey());
+        plusQuantity(shoppingCart, promotionQuantity, products);
+        int normalQuantity = promotionQuantity - shoppingCart.getValue();
+        promotionName = getPromotionName(promotionName, products);
+        int divide = getDivide(promotionName);
+        getRemainder(shoppingCart, normalQuantity, promotionQuantity, divide);
+    }
+
+    private boolean checkPromotionTime(LocalDate startDate, LocalDate endDate) {
+        LocalDate todayDate = DateTimes.now().toLocalDate();
+        return todayDate.isBefore(startDate) || todayDate.isAfter(endDate);
+    }
+
+    public void createReceipt(boolean isMembership) {
+
+    }
+
+    private void plusQuantity(Entry<String, Integer> shoppingCart, int promotionQuantity, List<Product> products) {
+        if (promotionQuantity > shoppingCart.getValue()) {
+            int divide = getDivide(getPromotionName(null, products));
+            int result = promotionQuantity % divide;
+            askPlusQuantity(shoppingCart, result, divide);
+        }
+    }
+
+    private void askPlusQuantity(Entry<String, Integer> shoppingCart, int result, int divide) {
+        if (result == divide - 1) {
+            outputView.addPromotionProduct(shoppingCart.getKey(), 1);
+            if (inputView.isY()) {
+                customer.addQuantity(shoppingCart.getKey());
+            }
+        }
+    }
+
+    private static String getPromotionName(String promotionName, List<Product> products) {
+        if (products.getFirst().getPromotion() != null) {
+            promotionName = products.getFirst().getPromotion();
+        }
+        return promotionName;
+    }
+
+    private int getDivide(String promotionName) {
+        Promotion promotion;
+        int divide = 0;
+        if (promotionName != null) {
+            promotion = promotions.get(promotionName);
+            int buy = promotion.getBuy();
+            int get = promotion.getGet();
+            divide = buy + get;
+        }
+        return divide;
+    }
+
+    private void getRemainder(Entry<String, Integer> shoppingCart, int normalQuantity, int promotionQuantity,
+                              int divide) {
+        int remainder = setRemainder(shoppingCart, promotionQuantity, normalQuantity, divide);
+        if (remainder != 0) {
+            outputView.promotionNotApplySomething(shoppingCart.getKey(), remainder);
+            if (inputView.isN()) {
+                customer.reduceQuantity(shoppingCart.getKey(), remainder);
+            }
+        }
+    }
+
+    private int setRemainder(Entry<String, Integer> shoppingCart, int promotionQuantity, int normalQuantity,
+                             int divide) {
+        if (normalQuantity >= 0) {
+            return shoppingCart.getValue() % divide;
+        }
+        int i = promotionQuantity % divide;
+        return (normalQuantity * -1) + i;
     }
 }
