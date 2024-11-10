9654b4f059dc3d4bbf81d70eb7a7110fbaa224f4
Dev-LHY <dev.leehy@gmail.com>
2024-11-10 17:23:56 +0900

    refactor(outputMessage): 불필요한 줄바꿈 삭제

--- src/main/java/store/view/OutputMessage.java
+++ src/main/java/store/view/OutputMessage.java
@@ -2,7 +2,7 @@ package store.view;
 
 public enum OutputMessage {
     WELCOME("안녕하세요. W편의점입니다." + System.lineSeparator() +
-            "현재 보유하고 있는 상품입니다." + System.lineSeparator() + System.lineSeparator()),
+            "현재 보유하고 있는 상품입니다." + System.lineSeparator()),
     INPUT_PRODUCT_NAME_AND_QUANTITY("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     ASK_MEMBERSHIP("멤버십 할인을 받으시겠습니까? (Y/N)"),
     ASK_REPURCHASE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
