7a003b5b2998847ef78770c5602175e8576b0bb9
Dev-LHY <dev.leehy@gmail.com>
2024-11-10 17:35:06 +0900

    feat(convenienceStoreController): 편의점 초기 설정 추가

--- src/main/java/store/controller/ConvenienceStoreController.java
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -1,10 +1,28 @@
 package store.controller;
 
+import java.util.List;
+import store.model.Product;
+import store.model.Promotion;
+import store.model.StockManager;
+import store.util.TextFileLoader;
 import store.view.InputView;
 import store.view.OutputView;
 
 public class ConvenienceStoreController {
     private final OutputView outputView = new OutputView();
     private final InputView inputView = new InputView();
+    private final StockManager stockManager = StockManager.getInstance();
+
+    private void initialize() {
+        try {
+            List<Product> products = Product.createProducts(TextFileLoader.productsLoader());
+            List<Promotion> promotions = Promotion.createPromotions(TextFileLoader.promotionsLoader());
+            stockManager.addAllProducts(products);
+            stockManager.addAllPromotions(promotions);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
+
 
