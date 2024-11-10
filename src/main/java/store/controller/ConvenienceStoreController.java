630ae4d7fbff8681a8785d1abcd04416c8c3b195
Dev-LHY <dev.leehy@gmail.com>
2024-11-10 17:34:02 +0900

    feat(convenienceStoreController): 편의점 컨트롤러 추가

---
+++ src/main/java/store/controller/ConvenienceStoreController.java
@@ -0,0 +1,10 @@
+package store.controller;
+
+import store.view.InputView;
+import store.view.OutputView;
+
+public class ConvenienceStoreController {
+    private final OutputView outputView = new OutputView();
+    private final InputView inputView = new InputView();
+}
+
